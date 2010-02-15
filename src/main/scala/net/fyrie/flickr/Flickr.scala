31fd305e33ada80bfe5212aef9a0f9e8be0cb21d
Derek Williams <derek@nebvin.ca>
2010-02-15 16:11:39 -0700

    Add flickr.auth.getFrob

--- src/main/scala/net/fyrie/flickr/Flickr.scala
+++ src/main/scala/net/fyrie/flickr/Flickr.scala
@@ -3,11 +3,14 @@ package net.fyrie.flickr
 import xml.{Node, XML}
 import dispatch._
 import net.liftweb.common._
+import java.security.MessageDigest
+
 
 abstract class Flickr {
   val endpoint = :/("api.flickr.com") / "services" / "rest"
 
   val apiKey: String
+  val apiSecret: String
 
   protected val http = new Http with Threads {
     override lazy val log: Logger = new Logger {
@@ -18,10 +21,10 @@ abstract class Flickr {
     }
   }
 
-  protected def get[T](method: String, params: Map[String,Any])(block: Seq[xml.Elem] => T): Box[T] =
+  protected def get[T](method: String, params: Map[String,Any])(block: Seq[xml.Elem] => Box[T]): Box[T] =
     http(endpoint <<? (params ++ Map("method" -> method, "api_key" -> apiKey)) <> {
       case rsp if (rsp \ "@stat").text == "ok" =>
-        Full(block(rsp.child.partialMap{case x: xml.Elem => x}))
+        block(rsp.child.partialMap{case x: xml.Elem => x})
       case rsp if (rsp \ "@stat").text == "fail" =>
         Failure("Flickr API Error "+(rsp \ "err" \ "@code").text+": "+(rsp \ "err" \ "@msg").text)
       case rsp =>
@@ -31,9 +34,30 @@ abstract class Flickr {
   object test {
     def echo(parameters: (String, String)*) =
       get("flickr.test.echo", Map(parameters:_*)){
-        _.map(x => (x.label, x.text)).toMap
+        result =>
+          Full(result.map(x => (x.label, x.text)).toMap)
       }
+  }
+
+  object auth {
+    def getFrob = {
+      get("flickr.auth.getFrob", Map("api_sig" -> md5SumString(apiSecret+"api_key"+apiKey+"methodflickr.auth.getFrob"))){
+        result =>
+          Box(result.map{
+            case <frob>{frob}</frob> => frob
+          }.headOption)
+      }
+    }
+  }
+
+  def md5SumString(bytes : String) : String = {
+    val md5 = MessageDigest.getInstance("MD5")
+    md5.reset()
+    md5.update(bytes.toArray.map(_.toByte))
+
+    md5.digest().map(0xFF & _).map { "%02x".format(_) }.foldLeft(""){_ + _}
   }
 
   def shutdown: Unit = http.shutdown
 }
+
--- src/test/scala/net/fyrie/flickr/Flickr.scala
+++ src/test/scala/net/fyrie/flickr/Flickr.scala
@@ -2,17 +2,27 @@ package net.fyrie.flickr
 package specs
 
 import org.specs._
+import net.fyrie.specs.matcher._
 
-class FlickrSpec extends Specification("Flickr API") {
+class FlickrSpec extends Specification("Flickr API") with BoxMatchers {
 
   val flickr = new Flickr with FlickrConfig
 
-  "api requests" should {
-    "succeed with echo test" in {
-      val response = flickr.test.echo("test param" -> "This is a test", "other test param" -> "another test")
-      response.toOption must beSome.which(_ must havePairs("test_param" -> "This is a test", "other_test_param" -> "another test"))
+  "flickr.test.echo" should {
+    "succeed" in {
+      val rsp = flickr.test.echo("test param" -> "This is a test", "other test param" -> "another test")
+      rsp must beFull.which(_ must havePairs("test_param" -> "This is a test", "other_test_param" -> "another test"))
+    }
+  }
+
+  "flickr.auth.getFrob" should {
+    "succeed" in {
+      val rsp = flickr.auth.getFrob
+      rsp must beFull
     }
   }
 
   doAfterSpec {flickr.shutdown}
+
 }
+
