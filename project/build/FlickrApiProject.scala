56248a2dbaba44cebe645673df581509662424b2
Derek Williams <derek@nebvin.ca>
2010-02-15 16:10:40 -0700

    Updated to sbt 0.7.0

--- project/build/FlickrApiProject.scala
+++ project/build/FlickrApiProject.scala
@@ -1,16 +1,19 @@
 import sbt._
 
 class FlickrApiProject(info: ProjectInfo) extends DefaultProject(info) {
+
   override def compileOptions = super.compileOptions ++ Seq(Unchecked)
 
-  val dispatchHttp = "net.databinder" %% "dispatch-http" % "0.7.+"
-  val liftCommon = "net.liftweb" % "lift-common" % "2.0-scala280-SNAPSHOT"
-  val slf4japi = "org.slf4j" % "slf4j-api" % "1.5.+"
-  
-  val logback = "ch.qos.logback" % "logback-classic" % "0.9.+" % "test"
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.+" % "test"
+  override def libraryDependencies =
+    Set("net.databinder" %% "dispatch-http" % "0.7.+",
+        "net.liftweb" % "lift-common" % "2.0-scala280-SNAPSHOT",
+        "org.slf4j" % "slf4j-api" % "1.5.+",
+        "ch.qos.logback" % "logback-classic" % "0.9.+" % "test",
+        "org.scala-tools.testing" %% "specs" % "1.6.+" % "test")
 
-  val snapshotRepo = "Scala Snapshots" at "http://scala-tools.org/repo-snapshots"
-  val databinder = "Databinder" at "http://databinder.net/repo"
+  override def repositories =
+    Set(ScalaToolsSnapshots,
+        "Databinder" at "http://databinder.net/repo")
+
 }
 
