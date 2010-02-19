b983b9606568a80de90a6089a7b61ada3452f821
Derek Williams <derek@nebvin.ca>
2010-02-19 14:43:25 -0700

    Can publish to a nexus repo now

--- project/build/FlickrApiProject.scala
+++ project/build/FlickrApiProject.scala
@@ -4,16 +4,17 @@ class FlickrApiProject(info: ProjectInfo) extends DefaultProject(info) {
 
   override def compileOptions = super.compileOptions ++ Seq(Unchecked)
 
-  override def libraryDependencies =
-    Set("net.databinder" %% "dispatch-http" % "0.7.+",
-        "net.liftweb" % "lift-common" % "2.0-scala280-SNAPSHOT",
-        "org.slf4j" % "slf4j-api" % "1.5.+",
-        "ch.qos.logback" % "logback-classic" % "0.9.+" % "test",
-        "org.scala-tools.testing" %% "specs" % "1.6.+" % "test")
+  val dispatchHttp = "net.databinder" %% "dispatch-http" % "0.7.0"
+  val liftCommon = "net.liftweb" % "lift-common" % "2.0-scala280-SNAPSHOT"
+  val slf4jApi = "org.slf4j" % "slf4j-api" % "1.5.10"
+  val logbackClassic = "ch.qos.logback" % "logback-classic" % "0.9.18" % "test"
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.3" % "test"
 
-  override def repositories =
-    Set(ScalaToolsSnapshots,
-        "Databinder" at "http://databinder.net/repo")
+  val scalaSnapshots = ScalaToolsSnapshots
+  val fyrieReleases = "Fyrie Releases" at "http://repo.fyrie.net/releases/"
 
+  override def managedStyle = ManagedStyle.Maven
+  val publishTo = "Fyrie Nexus" at "http://nexus.fyrie.net/content/repositories/releases/"
+  Credentials(Path.userHome / ".ivy2" / ".fyrie-credentials", log)
 }
 
