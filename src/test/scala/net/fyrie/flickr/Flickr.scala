56b3c40454849b8aa0db3e7d823631275b4f0811
Derek Williams <derek@nebvin.ca>
2010-02-22 14:33:36 -0700

    Fix leaking threaded flickr instances

--- src/test/scala/net/fyrie/flickr/Flickr.scala
+++ src/test/scala/net/fyrie/flickr/Flickr.scala
@@ -5,26 +5,25 @@ package specs
 import org.specs._
 import net.fyrie.specs.matcher._
 
-class Flickr extends flickr.Flickr with FlickrConfig
+object Flickr extends Flickr with FlickrConfig
 
 class FlickrSpec extends Specification("Flickr API") with BoxMatchers {
-
-  val flickr = new Flickr
+  import UserData._
 
   "flickr.test.echo" should {
     "succeed" in {
-      val rsp = flickr.test.echo("test param" -> "This is a test", "other test param" -> "another test")
+      val rsp = Flickr.test.echo("test param" -> "This is a test", "other test param" -> "another test")
       rsp must beFull.which(_ must havePairs("test_param" -> "This is a test", "other_test_param" -> "another test"))
     }
   }
 
   "flickr.auth.getFrob" should {
     "succeed" in {
-      val rsp = flickr.auth.getFrob
+      val rsp = Flickr.auth.getFrob
       rsp must beFull
     }
   }
 
-  doAfterSpec {flickr.shutdown}
+  doAfterSpec {Flickr.shutdown}
 
 }
